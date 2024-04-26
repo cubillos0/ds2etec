221421710fff83bc08fa8f4e68ac5c558ec92568
cubillos0 <yghorpereira12@outlook.com>
2024-04-25 22:34:17 -0300

    aula 25.04.24

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,13 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+function consultaCep(){ //criando função para consultar API
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
-
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+    let requisicao = new XMLHttpRequest(); // definindo os prarametros da requisição
+    let url = "https://viacep.com.br/ws/17054267/json/"; // colocando o link da api em JSON
+    requisicao.open("GET", url); // primeiramente fazendo a abertura do url
+    requisicao.send(); // envia a requisição para a API
+    requisicao.onload = function(){ // retorno quando a função foi executada com sucesso
+        alert(JSON.parse(requisicao.response).logradouro)
+    }
 }
+
+let botao = document.querySelector("#botao") // cria a váriavel em JS usando como base o ID "botão" no HTML
+botao.addEventListener("click", consultaCep) // aguarda o acionamento do botão para poder executar a função criada acima
