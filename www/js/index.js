be6b83f81759d773a0d13cbcc685de60c7cd5612
cubillos0 <yghorpereira12@outlook.com>
2024-04-26 22:28:19 -0300

    inserido navbar e adicionado mais campos utilizando a API

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,28 @@
 function consultaCep(){ //criando função para consultar API
 
+    let campo = document.querySelector("#cep") //criando variavel que puxa o texto do input de id 'cep'
     let requisicao = new XMLHttpRequest(); // definindo os prarametros da requisição
-    let url = "https://viacep.com.br/ws/17054267/json/"; // colocando o link da api em JSON
+    let url = "https://viacep.com.br/ws/"+ campo.value +"/json/"; // colocando o link da api em JSON
     requisicao.open("GET", url); // primeiramente fazendo a abertura do url
     requisicao.send(); // envia a requisição para a API
     requisicao.onload = function(){ // retorno quando a função foi executada com sucesso
-        alert(JSON.parse(requisicao.response).logradouro)
+        let objetoCEP = JSON.parse(requisicao.response) // criação de variáveis com query selector puxando os id do index.html
+        let campoEndereco = document.querySelector("#logradouro")
+        let campoLocalidade = document.querySelector("#localidade")
+        let campoComplemento = document.querySelector("#complemento")
+        let campoBairro = document.querySelector("#bairro")
+        let campoUF = document.querySelector("#uf")
+
+        // vinculando as variáveis criadas anteriormente com os campos da API
+        campoEndereco.value = objetoCEP.logradouro
+        campoLocalidade.value = objetoCEP.localidade
+        campoComplemento.value = objetoCEP.complemento
+        campoBairro.value = objetoCEP.bairro
+        campoUF.value = objetoCEP.uf
+
+        // only debug alert(objetoCEP.logradouro + ", " + objetoCEP.localidade + " - " + objetoCEP.uf + "\n" + objetoCEP.bairro +"\n" + objetoCEP.complemento)
+        // exibindo os campos da API assim que o botão de buscar for acionado
+        (campoEndereco + ", " + campoLocalidade + " - " + campoUF + "\n" + campoBairro +"\n" + campoComplemento)
     }
 }
 
